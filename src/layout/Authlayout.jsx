2b07a62d8362e131f19937103f0bf7462808a3b7
Mr-Deee <merchantdaniel8@gmail.com>
2023-03-09 13:29:49 +0000

    dropdown

--- src/layout/Authlayout.jsx
+++ src/layout/Authlayout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Sidebar from "../components/Sidebar";
+import Sidebar from "../components/Sidebar.jsx";
 import { Route } from "react-router-dom";
 import Header from "../components/Header";
 
