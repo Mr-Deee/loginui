166ee90d0f22e6a0a91ae86c773421fc4a2b4546
Mr-Deee <merchantdaniel8@gmail.com>
2023-03-09 12:50:23 +0000

    Inittial Commit With ERROR

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React from 'react';
+import{BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+
+
+
+}from "react-router-dom"
+import{adminRoutes, authRoutes}from"./routes/routes";
+import Authlayout from "./layout/Authlayout";
+import Adminlayout from "./layout/Adminlayout"
 import './App.css';
+const AppRoute = ({ component: Component, layout: Layout, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) => (
+      <Layout>
+        <Component {...props}></Component>
+      </Layout>
+    )}
+    exact
+  ></Route>
+);
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+<Router>
+  <Switch>
+  {authRoutes.map((route, idx) => (
+          <AppRoute
+            key={idx}
+            path={route.path}
+            component={route.component}
+            layout={Authlayout}
+          />
+        ))}
+        {adminRoutes.map((route, idx) => (
+          <AppRoute
+            key={idx}
+            path={route.path}
+            component={route.component}
+            layout={Adminlayout}
+          />
+        ))}
+        <Redirect strict from="/" to="/auth/signin" />
+
+
+
+
+
+
+
+  </Switch>
+
+
+
+
+</Router>
+
+
+  )
 }
 
 export default App;
---
+++ src/pages/auth/Signin.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// import SCLOGO from "../../assets/img/sc-logo.png";
+const Signin =()=>{
+return(
+    <React.Fragment>
+        
+    
+    
+    
+    </React.Fragment>
+);
+
+
+
+
+
+};
+export default Signin;
